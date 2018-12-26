4de999277736b4540bbd3997052ed99d3236e84d
Micah Carrick <micah@janrain.com>
2018-12-26 07:23:49 -0800

    Fix issue with deprecated GtkStock image. Use freedesktop.org named icon instead.

--- src/dee/entry.py
+++ src/dee/entry.py
@@ -1,5 +1,6 @@
 import os
 from xdg.DesktopEntry import DesktopEntry
+import gi
 from gi.repository import GdkPixbuf, Gtk
 
 def get_icon_pixbuf(icon, size):
@@ -19,20 +20,20 @@ def get_icon_pixbuf(icon, size):
             return pixbuf.scale_simple(size, size, GdkPixbuf.InterpType.NEAREST)
         except:
             pass
-    
-    default = icon_theme.load_icon(Gtk.STOCK_MISSING_IMAGE, size,
+
+    default = icon_theme.load_icon("image-missing", size,
                                       Gtk.IconLookupFlags.USE_BUILTIN)
     return default
 
 class Entry(DesktopEntry):
-    
+
     def __init__(self, filename=None):
         DesktopEntry.__init__(self, filename)
         self.is_modified = False
-    
+
     def isModified(self):
         return self.is_modified
-        
+
     def isReadOnly(self):
         """
         Return True if the entry's file is read-only for this user.
@@ -40,7 +41,7 @@ class Entry(DesktopEntry):
         if self.filename and not os.access(self.filename, os.W_OK):
             return True
         return False
-        
+
     def getIconPixbuf(self, size):
         """
         Render the icon to a GdkPixbuf for the icon at the specified sized.
